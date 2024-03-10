400c4f9614aa1a59d8c7be2a24e3a75aeb3da264
Cristian Hernán Del Río <cristianhernandelrio@Cristians-MacBook-Air.local>
2024-03-09 22:34:00 -0300

    borrado una linea que no iba en el archivo string y agregado de logs

--- ResumenPracticadeloexplicadoenlasclases/app/src/main/java/com/example/resumenpracticadeloexplicadoenlasclases/MainActivity.kt
+++ ResumenPracticadeloexplicadoenlasclases/app/src/main/java/com/example/resumenpracticadeloexplicadoenlasclases/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.resumenpracticadeloexplicadoenlasclases
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //LOGS
+        Log.d("tag debug", "soy un tag de debug")
+        Log.e("tag error", "soy un tag de error")
+        Log.i("tag info", "soy un tag de info")
+        Log.v("tag verbose", "soy un tag de verbose") //LOG VERBOSE
+        Log.w("tag warning", "soy un tag de warning")
     }
 }
